218f325a2e9eddbe8e8f40f8bfeac377cac8d5c9
vaibhav <vaibhav89000@gmail.com>
2021-05-20 17:51:58 +0530

    constructor

--- start.js
+++ start.js
@@ -1,7 +1,14 @@
 
-var User = {
-    email: 'vaibhav@gmail.com',
-    password: 'sharma'
+class User {
+    constructor(email,password){
+        this.email=email;
+        this.password=password;
+    }
+    login(){
+        console.log(`you have ${this.email} logged in successfuly`);
+    }
 }
 
-console.log(User.email);
+let user1 = new User('vaibhav','password');
+// console.log(user1);
+user1.login();
