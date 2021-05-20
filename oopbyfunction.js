06dfd99c47ec39c43c99023b49a55c98e7623124
vaibhav <vaibhav89000@gmail.com>
2021-05-20 18:06:06 +0530

    added logout method using prototype

--- oopbyfunction.js
+++ oopbyfunction.js
@@ -2,12 +2,17 @@ let User = function(email,password){
     this.email=email;
     this.password=password;
     this.login = function(){
-        console.log('login succesful');
+        console.log('login succesfull');
     }
+}
+
+//adding logout using prototype
+User.prototype.logout = function(){
+    console.log('logout done');
 }
 
 let user1= new User('vaibhav','password');
 console.log(user1.email,user1.password);
 user1.login();
 
-//adding logout using prototype
+user1.logout();
