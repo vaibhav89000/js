3d3706e36611d67517a99d8f60745fedf6fdbfaa
vaibhav <vaibhav89000@gmail.com>
2021-05-20 17:56:35 +0530

    inhertitance

--- start.js
+++ start.js
@@ -12,3 +12,14 @@ class User {
 let user1 = new User('vaibhav','password');
 // console.log(user1);
 user1.login();
+
+// inheritance of user class
+class Admin extends User {
+    assigntask(){
+        console.log('The task is assigned by admin');
+    }
+}
+
+let admin1 = new Admin('vaibhav','password');
+console.log(admin1.email,admin1.password);
+admin1.assigntask();
